00543e7a9cd6c192a20a124f80afbc9f556180ac
Qubit1010 <hassanaleem86@gmail.com>
2024-10-11 14:07:32 +0500

    add doctor frontend created

---
+++ admin/src/App.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import Login from "./pages/login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import NavBar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointment from "./pages/Admin/AllApointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
+
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <NavBar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllApointment />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <div className="">
+      <Login />
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default App;
